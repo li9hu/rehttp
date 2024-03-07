07b5aa947d1476f2b9723d08364c8c299adf852c
ewanLi <ewan9hu@gmail.com>
2024-03-07 11:54:55 +0800

    init

---
+++ rehttp.go
@@ -0,0 +1,153 @@
+package rehttp
+
+import (
+	"bytes"
+	"github.com/hashicorp/go-retryablehttp"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type HTTP struct {
+	Client        *http.Client
+	ResponseLimit int64
+	Timeout       int64
+	RetryMax      int
+}
+
+var DefaultClient = NewDefault()
+
+// NewDefault 默认初始化的HTTP
+func NewDefault() *HTTP {
+	var defaultHTTP = &HTTP{
+		Client:        initHTTP(10, 3),
+		ResponseLimit: 0,
+	}
+	return defaultHTTP
+}
+
+// New 创建HTTPClient
+func New(http *HTTP) *HTTP {
+	h := &HTTP{
+		Client:        initHTTP(http.Timeout, http.RetryMax),
+		ResponseLimit: http.ResponseLimit,
+	}
+	return h
+}
+
+// Post 发送POST请求
+func (h *HTTP) Post(urlStr string, headers map[string]string, requestBody []byte) *Result {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return &Result{URL: urlStr, Err: err}
+	}
+
+	startTime := time.Now()
+	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(requestBody))
+	if err != nil {
+		return &Result{URL: u.String(), Err: err}
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	resp, err := h.Client.Do(req)
+	if err != nil {
+		return &Result{URL: u.String(), Err: err}
+	}
+	defer func() {
+		if resp != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	// 读取响应结果，有长度限制则截取部分内容
+	var bodyReader io.Reader = resp.Body
+	if h.ResponseLimit != 0 {
+		bodyReader = io.LimitReader(resp.Body, h.ResponseLimit)
+	}
+	b, err := io.ReadAll(bodyReader)
+	if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
+		return &Result{URL: u.String(), Err: err}
+	}
+
+	// 执行花费时间
+	durationSeconds := time.Since(startTime).Seconds()
+
+	return &Result{
+		URL:          u.String(),
+		ResponseBody: string(b),
+		StatusCode:   resp.StatusCode,
+		Duration:     durationSeconds,
+	}
+}
+
+func Post(urlStr string, headers map[string]string, requestBody []byte) *Result {
+	result := DefaultClient.Post(urlStr, headers, requestBody)
+	return result
+}
+
+// Get 发送GET请求
+func (h *HTTP) Get(t string, headers map[string]string) *Result {
+	u, err := url.Parse(t)
+	if err != nil {
+		return &Result{URL: t, Err: err}
+	}
+
+	startTime := time.Now()
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return &Result{URL: u.String(), Err: err}
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	resp, err := h.Client.Do(req)
+	if err != nil {
+		return &Result{URL: u.String(), Err: err}
+	}
+	defer func() {
+		if resp != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	// 读取响应结果，有长度限制则截取部分内容
+	var bodyReader io.Reader = resp.Body
+	if h.ResponseLimit != 0 {
+		bodyReader = io.LimitReader(resp.Body, h.ResponseLimit)
+	}
+	b, err := io.ReadAll(bodyReader)
+	if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
+		return &Result{URL: u.String(), Err: err}
+	}
+
+	// 执行花费时间
+	duration := time.Since(startTime).Seconds()
+
+	return &Result{
+		URL:          u.String(),
+		ResponseBody: string(b),
+		StatusCode:   resp.StatusCode,
+		Duration:     duration,
+	}
+}
+
+func Get(t string, headers map[string]string) *Result {
+	result := DefaultClient.Get(t, headers)
+	return result
+}
+
+// 自带请求重试的HTTPClient
+func initHTTP(timeout int64, retryMax int) *http.Client {
+	retryClient := retryablehttp.NewClient()
+	retryClient.Logger = nil
+	retryClient.RetryMax = retryMax
+	client := retryClient.StandardClient()
+	client.Timeout = time.Duration(timeout) * time.Second
+	return client
+}
---
+++ types.go
@@ -0,0 +1,16 @@
+package rehttp
+
+type Result struct {
+	StatusCode   int     `json:"statusCode,omitempty"`
+	URL          string  `json:"URL,omitempty"`
+	Duration     float64 `json:"duration,omitempty"`
+	ResponseBody string  `json:"responseBody,omitempty"`
+	Err          error   `json:"err,omitempty"`
+}
+
+type Request struct {
+	Data   []byte
+	URL    string
+	Header map[string]string
+	Method string
+}
